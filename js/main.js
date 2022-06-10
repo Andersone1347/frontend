0b77bd86aa5bd7a40e7285e7ea5ecc691b60ddcb
Andersone1347 <Panov.deadman@yandex.ru>
2022-06-10 13:01:13 +0300

    39 урок

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
-let name = 5;
+/*  37 урок */
+/* let name = 5;
 let str = "10";
 let boolean = true;
 let boolean2 = false;
 console.log("Текст 1");
-//console.log(5+8);
+console.log(5+8); */
+
+/* 38 Урок */
+
+/* let a = 7;
+console.log("Text\non 2 line" + a ); */
+
+          /*   проверка числа , рандом */
+/*  function isNumber(n) {
+    return !isNaN(parseFloat(n)) && !isNaN(n - 0)
+}
+function randomInteger(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+console.log(randomInteger(1,5)) 
+ */
+
+/* 39 урок */
+let arr = [5,2,"Str", true]
