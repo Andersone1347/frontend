c9f3d192f3ef95b78b8390ded2d0bc9150a1ea3b
Andersone1347 <Panov.deadman@yandex.ru>
2022-06-16 12:10:12 +0300

    редми

--- js/main.js
+++ js/main.js
@@ -15,17 +15,17 @@ console.log(5+8); */
 /* 38 Урок */
 
 /* let a = 7;
-console.log("Text\non 2 line" + a ); */
+console.log("Text\non 2 line "+a ); */
 
           /*   проверка числа , рандом */
-/*  function isNumber(n) {
+ function isNumber(n) {
     return !isNaN(parseFloat(n)) && !isNaN(n - 0)
 }
 function randomInteger(min, max) {
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
 }
-console.log(randomInteger(1,5))  */
+console.log(isNumber(5)) 
 
 
 /* 39 урок */
